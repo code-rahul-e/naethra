41b64a3c555aeae4e50336ea974a9a5e37969496
Rahul <89511755+code-rahul-e@users.noreply.github.com>
2024-10-06 14:02:03 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// nav menu
+let menu = document.querySelector("#menu-logo");
+let box = document.querySelector(".menus");
+
+
+menu.onclick = function(){
+    box.classList.toggle("menu-active");
+}
+
+// slider images
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
